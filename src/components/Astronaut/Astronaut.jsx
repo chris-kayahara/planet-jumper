1efcd893c2ad4b0eb2c3f75e0ad7d5288190d964
Austin Croucher <austin.croucher@gmail.com>
2023-03-17 09:30:38 -0400

    added notes to astronaut to better explain component

--- src/components/Astronaut/Astronaut.jsx
+++ src/components/Astronaut/Astronaut.jsx
@@ -4,6 +4,7 @@ import AstroImg from '../../assets/Images/SpaceAvatar.png';
 
 function Astronaut(props, planet) {
     const [isJumping, setIsJumping] = useState(false);
+    //state here keeps track of wether the astronaut is jumping or not 
 
     useEffect(() => {
         const handleKeyPress = (event) => {
@@ -18,6 +19,10 @@ function Astronaut(props, planet) {
             window.removeEventListener("keydown", handleKeyPress);
         };
     }, []);
+    //this use effect listens for a keydown press of space bar in order to make the astronaut jump and passes the planet.gravity
+    //which will inherently change the jump height of the avatar depedning on the gravity data that is passed. If no planet gravity
+    //is passed we used the const earthGravity as a default  
+
 
     const baseJumpHeightOrVelocity = 100; // Adjust this value to set the base jump height or velocity
     const earthGravity = 9.81;
@@ -27,6 +32,11 @@ function Astronaut(props, planet) {
 
     const baseTransitionDuration = 0.5; // Adjust this value to set the base transition duration (in seconds)
     const transitionDuration = baseTransitionDuration * (earthGravity / gravity);
+
+
+    //this function sets the isjumping to true which then triggers the inline CSS below in order to transition and
+    //animate the astronaut upwards depending on the passed through gravity. After the jump animation is complete
+    //is jumping gets set to false which returns the image to the default position
 
     const jumpAvatar = () => {
         if (!isJumping) {
